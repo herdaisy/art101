87546ee06ca739fda3a70fe60df8281687066f84
herdaisy <hdaisy650@gmail.com>
2024-10-25 16:58:42 -0700

    Create lab.js

---
+++ lab6/lab 7/lab.js
@@ -0,0 +1,23 @@
+// index.js - purpose and description here
+// Author: Daisy Herrera
+// Date: 10/17/24
+
+// sortUserName-a function that takes user input and sorts the letters
+//of their name 
+function sortname() 
+  Var userName=window.prompt("Hello.");
+  console.log ("userName=", userName);
+  //split string to arry
+  var nameArry=userName.split('');
+  console.log("nameArray.sort=", nameArraySort);
+  //split string to arry
+  var nameArraySort= nameArray.sort();
+  //join array back to a string
+  var namesorted=nameArraysort.join('')
+  console.log("namesorted=", namesorted);
+  //userName.tolower(). Split(" ").sort().join("")
+  return namesorted;
+
+  //output 
+  document.written("Daisy": ",
+    sortUserName(),"</br>"); 
