80f984d3d92735ae0f7bed2d4afa65cd529abd9b
herdaisy <hdaisy650@gmail.com>
2024-10-28 21:33:17 -0700

    Update lab.js

--- lab6/lab 7/lab.js
+++ lab6/lab 7/lab.js
@@ -3,20 +3,26 @@
 // Date: 10/26/24
 
 
-// sortUserName-a function that takes user input and sorts the letters
-//of their name 
 function SortUsername() {
   var userName = window.prompt("Hi, please type in your full name so I can fix it.");
   console.log ("userName =", userName);
-  //split string to arry
+
+  //turn string to arry
   var nameArry=userName.split('');
   console.log("nameArray=", nameArray);
+
   //Sort array
   var nameArraySort= nameArray.sort();
-  //join array back to a string
-  var namesorted=nameArraysort.join('')
-  console.log("namesorted=", namesorted);
-  return namesorted;
+  console.log("nameArrySort=", nameArraySort);
+
+  //array back to a string
+  var namesorted=nameArraysort.join('');
+  console.log("namesorted=", nameSorted);
+
+  return "Your sorted names is: " + namesorted;
 }
-  //output 
-  document.writeln("oh hey, I've fixed your name:", sortUserName(),"</br>");
+function displaysortedName () {
+  let result= sortusename ();
+  document.getElementById("output").innerHTML='<p>I fixed your name: <strong>${result}</strong<>/p>;
+  }
+  window.onload=displaysortedName;
