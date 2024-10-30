ce8cff0738922742de5966a55b9b997ad6b60900
herdaisy <hdaisy650@gmail.com>
2024-10-29 18:44:39 -0700

    Update lab.js

--- lab7/lab.js
+++ lab7/lab.js
@@ -2,26 +2,28 @@
 // Author: Daisy Herrera
 // Date: 10/26/24
 
-function sortUsername() {
+function sortUserName() {
   var userName = window.prompt("Hi, please type in your full name so I can fix it.");
   console.log("userName =", userName);
-
-  // Turn string to array
-  var nameArray = userName.split('');
-  console.log("nameArray=", nameArray);
-
+  
+  // Turn string into array
+  var nameArray = userName.split(' ');
+  console.log("nameArray =", nameArray);
+  
   // Sort array
   var nameArraySort = nameArray.sort();
-  console.log("nameArrySort=", nameArraySort);
-
-   // Array back to a string
-   var namesorted=nameArraySort.join('');
-   console.log("namesorted=", namesorted);
- 
-   return "Your sorted names is: " + namesorted;
- }
- function displaysortedName () {
-   let result= sortUsername ();
-   document.getElementById("output").innerHTML="<p>I fixed your name: <strong>${result}</strong></p>";
+  console.log("nameArraySort =", nameArraySort);
+  
+  // Array back to string
+  var nameSorted = nameArraySort.join(' ');
+  console.log("nameSorted =", nameSorted);
+  
+  return "Your sorted names are: " + nameSorted;
 }
-window.onload=displaySortedName;
+
+function displaySortedName() {
+  let result = sortUserName();
+  document.getElementById("output").innerHTML = `<p>I fixed your name: <strong>${result}</strong></p>`;
+}
+
+window.onload = displaySortedName;
