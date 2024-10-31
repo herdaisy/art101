303a8dac0c9faa2533a957044311351b358bf0ea
herdaisy <hdaisy650@gmail.com>
2024-10-30 18:39:19 -0700

    kkop

--- lab8/lab.js
+++ lab8/lab.js
@@ -1,25 +1,31 @@
+// index.js- Experiment with anon functions and callbacks 
 /*
 * Author: Daisy 
-* created Octor 31
-* License:Public Domain
+* created October 31
+* License: Public Domain
 */
 
+// Function to check if a number is even
 function isEven(x){ 
-    return (x % 2== 0); 
+    return (x % 2 == 0); 
 }  
- //test function
- console.log("Is 1 even? ", isEven(1));
- console.log("Is 2 even? ", isEven(2));
 
- arry=[100,81,4,16,42,144,10000]
- console.log("My array", array);
- 
- var results=array.map(isEven);
-//should return[true,false,true,true,true,true,true]
-console.log("Test of evenness of array:", results);
+// Test function
+console.log("Is 1 even? ", isEven(1));
+console.log("Is 2 even? ", isEven(2));
 
-var result=array.map(function(x){
+// Declare and initialize the array
+var array = [100,81,4,16,42,144,10000];
+console.log("My array", array);
+
+// Map over the array to get evenness results
+var results = arry.map(isEven);
+// Should return [true,false,true,true,true,true,true]
+console.log("Test of evenness of arry:", results);
+
+// Map over the arry to calculate square root for each element using an anonymous function
+var result = arry.map(function(x){
     return x ** 0.5;
-        })
-     //should return [10,9,2,4,6.40, 6.48074069840786,12,100]
-   console.log("Squareroot of array:",result);
+ });
+// Should return [10,9,2,4,,6.,12,.]
+ console.log("Squareroot of arr:",result);
