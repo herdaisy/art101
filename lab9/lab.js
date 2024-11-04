36f7fdec0fd9885e1262f3853bf8db33af7177a4
herdaisy <hdaisy650@gmail.com>
2024-11-04 13:33:45 -0800

    iop

--- lab9/lab.js
+++ lab9/lab.js
@@ -4,15 +4,15 @@ lab.js - This simple JavaScript/jQuery script uses buttons to modify some elemen
 
 Requirements: jQuery must be loaded for this script to work.
 
-Author: Wes Modes
+Author: Daisy Herrera
 Date: 2024
 */
 
 //add button to challenge section 
-S("#challenge").append("<button id=button-challenge'>Make Special</button>");
+$("#challenge").append("<button id='button-challenge'>Make Special</button>");
 
 //add a click listener to the challenge button 
-S("button-challenge").click(function(){
-    //now ad (or subtract) "the special" class to the section
+$("button-challenge").click(function(){
+    //now ad (or subtract) the "special" class to the section
     $("#challenge").toggleClass("special");
 });
