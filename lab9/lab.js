c2f94b13b692ce96aaf436f5ed5e23c2e662b38f
herdaisy <hdaisy650@gmail.com>
2024-11-03 21:05:49 -0800

    Update lab.js

--- lab9/lab.js
+++ lab9/lab.js
@@ -1,18 +1,33 @@
-<head>
+
+
+return 
+    
+<html> 
+<head/>
+
+   /<meta charset="UTF-8"/>
+   <meta name="viewport" content="width=device-width,inital-scale=1.0"/>
+ 
+    <title> Lab 9=Libraries & jQuery </title>
+    <link rel="stylesheet" type="text/css" href="../css/site.css" />
+    <link rel="stylesheet" type="text/css" href="css/index.css" />
+    <script src="http://code.jquery.com/jquery-3.7.1.min.js"></script>
+<head/>
+
+<body>
 
 <div id="challenge">
-    <p>There are more steps involved that have made the assignements harder,
-   I find myself troubleshotting more</p>
+   <p>There are more steps involved that have made the assignments harder, I find myself troubleshooting more</p>
+ </div>
 
-<div id="problems">
-<p>I am having probelms trying to figure out how to run it smoothly</p>
+<div id = "problems">
+   <p>I am having problems trying to figure out how to run it smoothly</p>
+ </div>
 
-<div id="results">
-    <p>Overall, I tired my best to figure out the issues and follow the prompt the best I could</p>
-</div>
-
-<link rel="stylesheet" type="text/css" href="../css/site.css">
-<link rel="stylesheet" type="text/css" href="css/index.css">
-<script src="http://code.jquery.com/jquery-3.7.1min.js"></script> 
-</head>
+<div id = "results">
+   <p>Overall, I tried my best to figure out the issues and follow the prompt the best I could </p>
+   </div>
+   </body>
+   </html>
+;
 
