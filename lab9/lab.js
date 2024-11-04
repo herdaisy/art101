77cee31c7846cdc8fe8598107e17040020c8a007
herdaisy <hdaisy650@gmail.com>
2024-11-03 20:01:39 -0800

    fixed
    wow

---
+++ lab9/lab.js
@@ -0,0 +1,18 @@
+<head>
+
+<div id="challenge">
+    <p>There are more steps involved that have made the assignements harder,
+   I find myself troubleshotting more</p>
+
+<div id="problems">
+<p>I am having probelms trying to figure out how to run it smoothly</p>
+
+<div id="results">
+    <p>Overall, I tired my best to figure out the issues and follow the prompt the best I could</p>
+</div>
+
+<link rel="stylesheet" type="text/css" href="../css/site.css">
+<link rel="stylesheet" type="text/css" href="css/index.css">
+<script src="http://code.jquery.com/jquery-3.7.1min.js"></script> 
+</head>
+
