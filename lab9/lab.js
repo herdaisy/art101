90176de5030a3d02ba165a91654dcf956a5df2df
herdaisy <hdaisy650@gmail.com>
2024-11-06 15:27:37 -0800

    23

--- lab9/lab.js
+++ lab9/lab.js
@@ -11,8 +11,12 @@ Date: 2024
 //add button to challenge section 
 $("#challenge").append("<button id='button-challenge'>Make Special</button>");
 
+
 //add a click listener to the challenge button 
 $("button-challenge").click(function(){
     //now ad (or subtract) the "special" class to the section
     $("#challenge").toggleClass("special");
 });
+
+//add button problem 
+
