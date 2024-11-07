78e46916893397248129f90f9000bb5dd082553f
herdaisy <hdaisy650@gmail.com>
2024-11-06 23:24:47 -0800

    101

--- lab10/lab.js
+++ lab10/lab.js
@@ -10,7 +10,7 @@ function generateRandomText() {
     const text = "Lorem pop.";
     const min= 3;
     const max=100;
-    const randlen= Math.floor(math.random()* (max-min+1))+ min;
+    const randlen= Math.floor(Math.random()* (max-min+1))+ min;
     //Get a random starting index to slice the Lorem Ispsum text
     const randStart= Math.floor(Math.random()* (text.length-randlen+1));
     //Generate the random Lorem Ispsum-like text
@@ -18,6 +18,6 @@ function generateRandomText() {
 }
 
 //get new fake dialogue
-const newTest = generateRandomTest();
+const newTest = generateRandomText();
 //append a new to our output div
 $("#output").append('<div class="text"><p>' + newTest + '</p></div>');
