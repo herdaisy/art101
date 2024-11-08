d69d894ed55f41a7a7de51ca2b15547926b1eb3b
herdaisy <hdaisy650@gmail.com>
2024-11-07 16:58:19 -0800

    12
    12

--- lab10/lab.js
+++ lab10/lab.js
@@ -9,7 +9,8 @@ Date:2024
 function generateRandomText() {
     const text =
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
-    const min = 3;
+        const newText= " "
+      const min = 3;
     const max = 100;
     const randLen = Math.floor(Math.random() * (max - min + 1)) + min;
      //Get a random starting index to slice the Lorem Ispsum text
@@ -25,13 +26,11 @@ function generateRandomText() {
       $("#output").append('<div class="text"><p>' + newText+ "</p></div>");
   });
   //append a new div to our output div
-  //append a new div to our output div
   $("#send-user-input").click(function () {
       //Get user's input 
   const userInput= $("user-input").Val();
       //Append the user's text as a right-aligned message bubble 
-  $("#output")
-        .append('<div class="text right"><p>' + userInput+ '</p></div>');
+  $("#output") .append('<div class="text right"><p>' + userInput+ '</p></div>');
   //clear the input field after sending 
   $("#user-input").val("");
-  });
+  }); 
