3dd744ab55cd5af3d0f7651ddc3a897a9a88a42f
herdaisy <hdaisy650@gmail.com>
2024-11-18 19:57:14 -0800

    ko

--- Lab13/lab.js
+++ Lab13/lab.js
@@ -1,27 +1,30 @@
-/*
-lab.js - This simple JavaScript/jQuery script gets a value from an input field and outputs a modified verison.
 
-Requirements: jQuery must be loaded for this script to work.
+// Lab 13FizzBuzz with Loop of Number 1-200
 
-Author: Daisy Herrera
-Data: 11/18/2024
+function fizzBuzz() {
 
-*/
-
-function fizzBuzzBoom() {
-    //makes a loop
-    for (let i = 1; i <= 200; i++) {
-        let output= ' ';
-    //if the number is a mulitiple of 3 outputs fizz
-    if (i % 3 === 0) output += "Fizz!";
-    //if the number is a muliple of 5 output buzz
-    if (i % 5 === 0) ouput += "Buzz!";
-    //if number is a multiple of 7 output boom
-     if(i % 7 === 0) output += "Boom!";
+    // Initialize an empty string to store the result
+    var resultString = "";
     
-     $("#output").append( '<p>' + (output || i) +' </p>');
-     
-      }
-}
-
-fizzBuzzBoom();
+    for (var num = 1; num <= 200; num++) {
+            var str = "";
+        if (num %3 ===0) {
+            str += "Fizz";
+        }
+    if (num %5 ===0) {
+        str += "Buzz"
+    }
+    if (num %7 ===0) {
+    str +="Boom"
+    }
+    
+    
+    if(str===""){
+    str=num;
+    }
+    resultString += "<p>" + str + "</p>\n";
+    }
+    
+    $("#output").html(oneLongString);
+    
+    }
